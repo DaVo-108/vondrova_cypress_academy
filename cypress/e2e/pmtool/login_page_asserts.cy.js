dfa7bd7f3a9630a09e6cf33b7041360c059f2de5
Daniela Vondrová <daniela.jurasikova108@gmail.com>
2025-08-18 21:02:19 +0200

    Mocha, chai

---
+++ cypress/e2e/pmtool/login_page_asserts.cy.js
@@ -0,0 +1,10 @@
+import { LoginPage } from "../../page-objects/pmtool/login_page";
+
+describe("Login page tests", () => {
+  beforeEach(() => {
+    new LoginPage().openPmtool();
+  });
+  it("Page header has text 'Login'", () => {
+    new LoginPage().pageHeaderHasText("Login");
+  });
+});
