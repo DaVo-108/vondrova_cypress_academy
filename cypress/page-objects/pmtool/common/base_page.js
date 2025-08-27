6a94743a5748b47570286250f6739a9355f4bbc5
Daniela Vondrová <daniela.jurasikova108@gmail.com>
2025-08-27 21:00:11 +0200

    Atomic Tests

---
+++ cypress/page-objects/pmtool/common/base_page.js
@@ -0,0 +1,10 @@
+export class BasePage {
+  constructor(path) {
+    this.baseURL = "https://tredgate.com/pmtool/index.php?";
+    this.path = path;
+  }
+
+  visit() {
+    cy.visit(this.baseURL + this.path);
+  }
+}
