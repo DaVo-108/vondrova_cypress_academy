5301be2b969b3955ea631219a2d52b8a35af0f16
Daniela Vondrová <daniela.jurasikova108@gmail.com>
2025-09-29 19:43:31 +0200

    say goodbay and reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    embeddedScreenshots: true,
+    showSkipped: true,
+    inlineAssets: true,
+    json: true,
+  },
+  screenshotsFolder: "images",
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
     experimentalOriginDependencies: true,
     env: {
@@ -14,8 +23,11 @@ module.exports = defineConfig({
       eshop_url: "https://tredgate.com/eshop",
       webtrain_url: "https://tredgate.com/webtrain",
       automation_test_store_url: "https://automationteststore.com/",
+      tegb_cert_url: "https://tegb-frontend-88542200c6db.herokuapp.com/",
+      TEST_LANGUAGE: process.env.TEST_LANGUAGE || "en",
     },
     watchForFileChanges: false,
     defaultCommandTimeout: 10000,
+    screenshotOnRunFailure: true,
   },
 });
